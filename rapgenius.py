11a7b9e83d67248e9cd5e38437f3a88d223b05ec
Patrick Conner <pconner03@gmail.com>
2013-12-22 21:23:14 -0500

    Fixed - to an extent - artists searches that redirect to artist page. Started using actual objects

--- rapgenius.py
+++ rapgenius.py
@@ -8,27 +8,54 @@ import re
 
 #TODO - this
 class artist:
-	name = ""
-	popularSongs = []
-	songs = []
+
+	def __init__(self, name, url):
+		self.name = name
+		self.url = url
+
+		self.popularSongs = [] #array of 'popular songs' from artist's page. Initially empty.
+		self.songs = [] #array of songs on artist page that are not listed as 'popular songs.' Initially empty.
+	
+	#instantiates object's popular song array and returns it
+	def getPopularSongs(self):
+		self.popularSongs = getArtistPopularSongs(self.url)
+		return self.popularSongs
+
+	def getAllSongs(self):
+		self.songs = getArtistSongs(self.url)
 
 
 class song:
-	name = ""
-	link = ""
-	artists = ""
-	lyrics = ""
 
+	def __init__(self, name, url):
+		self.name = name
+		self.url = url
+		self.rawLyrics = ""
+		#TODO - lyric + annotation stuff
+	def getRawLyrics(self):
+		self.rawLyrics = getLyricsFromUrl(self.url)
+		return self.rawLyrics
 
 #Searches for 'artist', returns list of matches
 #Currently list of strings - TODO - change to objects
-def searchArtist(artist):
-	searchUrl = RAPGENIUS_SEARCH_URL+'?q='+searchUrlFormat(artist)
+def searchArtist(artistName):
+	searchUrl = RAPGENIUS_SEARCH_URL+'?q='+searchUrlFormat(artistName)
 	soup = BeautifulSoup(urlopen(searchUrl).read())
 	results = []
-	for row in soup.find_all('a', href=re.compile('/artists/.')):
-		results.append((''.join(row.findAll(text=True)), RAPGENIUS_URL+row.get('href')))
+	artistRe = re.compile('/artists/.')
+
+	#if exact artist name is entered, rapgenius redirects to artist page
+	if re.match('/artists/[0-9]*/follows', soup.find('a', href=artistRe).get('href')):
+		#TODO - get actual artist name from artist url (to ensure proper capitalization, etc)
+		results.append(artist(artistName, getArtistUrl(artistName)))
+
+	for row in soup.find_all('a', href=artistRe):
+		#print ''.join(row.findAll(text=True)) + RAPGENIUS_URL+row.get('href')
+		results.append(artist(''.join(row.findAll(text=True)), RAPGENIUS_URL+row.get('href')))
 	return results
+
+def getArtistUrl(artistName):
+	return RAPGENIUS_ARTIST_URL+"/" + artistName.replace(" ","-")#TODO - figure out other character replacements
 
 #converts search query into something that can be put into search URL 
 def searchUrlFormat(query):
@@ -41,7 +68,7 @@ def searchSong(song):
 	songs = []
 	for row in soup.find_all('a', class_='song_link'): #force to ignore 'hot song' results
 		if(row.parent.get('class')!=None):
-			songs.append((''.join(row.findAll(text=True)).strip(), RAPGENIUS_URL+row.get('href')))
+			songs.append(song(''.join(row.findAll(text=True)).strip(), RAPGENIUS_URL+row.get('href')))
 	#TODO - object model
 
 	#print songs
@@ -64,10 +91,21 @@ def getArtistPopularSongs(url):
 	songs = []
 	for row in soup.find('ul', class_='song_list'):
 		if(type(row.find('span'))!=int):
-			songs.append((''.join(row.find('span').findAll(text=True)).strip(), RAPGENIUS_URL+row.find('a').get('href')))
+			songs.append(song(''.join(row.find('span').findAll(text=True)).strip(), RAPGENIUS_URL+row.find('a').get('href')))
 			
 	return songs
 
+def getArtistSongs(url):
+	#TODO - implement this
+	#For now, just return popular songs
+	return getArtistPopularSongs(url)
+
 def test():
-	s = searchSong("Outkast Atliens")
-	print getLyricsFromUrl(s[0][1])
+	outkast = searchArtist("Outkast")[0]
+	#print outkast.name
+	#print outkast.url
+	outkast.getPopularSongs()
+	for song in outkast.popularSongs:
+		print song.getRawLyrics()
+
+
